781265dcf1ee7be05405607612134dd0aeba1bd4
unknown <2869210303@qq.com>
2023-05-02 14:18:03 +0800

    史上最强cpp有理数类，没有之一(1)

--- RationalTest.cpp
+++ RationalTest.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include "Rational.h"
-#include "Matrix.h"
 using namespace std;
 
 int main()
 {
-	// ÓĞÀíÊı²âÊÔ
+	// æœ‰ç†æ•°æµ‹è¯•
 	Rational r1(1.5);
 	Rational r2(7.5);
 	cout << r1 + 1 << endl;
